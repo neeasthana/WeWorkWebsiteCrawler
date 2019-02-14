6015611cfb32d2343cd63f47e3066e196a50f1af
neeasthana <neeasthana@gmail.com>
2019-02-13 22:10:42 -0500

    Commiting basic structure for classes

---
+++ src/Crawler.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author neeraj
+ *
+ */
+public class Crawler {
+	private static int NUM_CONCURRENT_REQUESTS = 20;
+	
+	/**
+	 * 
+	 */
+	public ArrayList<String> readWebsites(){
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param url - url to be parsed
+	 */
+	private String getWebsiteHTML(String url) {
+		return null;
+	}
+}
