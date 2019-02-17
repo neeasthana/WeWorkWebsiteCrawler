b17f2f8067e6ce14ae2ecc47d9a378c3b4aa4b9d
neeasthana <neeasthana@gmail.com>
2019-02-16 23:33:09 -0500

    adding simple test cases for website searcher

--- src/WebsiteSearcher.java
+++ src/WebsiteSearcher.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.concurrent.Semaphore;
+
 /**
  * Retreives and parses HTML for a website URL
  * @author neeraj
@@ -5,20 +11,66 @@
  */
 public class WebsiteSearcher extends Thread{
 	
-	private String url;
+	private Semaphore semaphore;
+	
+	private String website;
 	
 	private String searchTerm;
 	
-	public WebsiteSearcher(String url, String searchTerm) {
-		this.url = url;
+	/**
+	 * 
+	 * @param semaphore
+	 * @param url
+	 * @param searchTerm
+	 */
+	public WebsiteSearcher(Semaphore semaphore, String website, String searchTerm) {
+		this.semaphore = semaphore;
+		this.website = website;
 		this.searchTerm = searchTerm;
 	}
 	
-	private String getHTML() {
-		return null;
+	public String getHTML() throws IOException {
+		URL url = new URL(website);
+		
+		BufferedReader reader = null;
+		
+		try {
+			// acquire semaphore if available or else what till it is available
+			semaphore.tryAcquire();
+			
+			String line;
+			StringBuffer html = new StringBuffer();
+					
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			// read the website html line by line
+			while((line = reader.readLine()) != null) {
+				html.append(line);
+			}
+			
+			return html.toString();
+		}
+		finally {
+			reader.close();
+			semaphore.release();
+		}
 	}
 	
-	public boolean checkIfSearchTermExistsOnWebsite() {
+	public boolean checkIfSearchTermExistsOnWebsite() throws IOException {
+		System.out.println(getHTML());
 		return false;
+	}
+	
+	public static void main(String[] args) {
+		Semaphore s = new Semaphore(1);
+		
+		WebsiteSearcher searcher = new WebsiteSearcher(s, "https://www.yahoo.com", "something");
+		
+		try {
+			searcher.checkIfSearchTermExistsOnWebsite();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/test/WebsiteSearcherTest.java
@@ -0,0 +1,23 @@
+package test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WebsiteSearcherTest {
+
+	@Test
+	void testWebsiteSearcher() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetHTML() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckIfSearchTermExistsOnWebsite() {
+		fail("Not yet implemented");
+	}
+
+}
