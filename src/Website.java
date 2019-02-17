07851187f8a32c8fa35502f638ff0dab449141bb
neeasthana <neeasthana@gmail.com>
2019-02-17 00:06:24 -0500

    adding base class definition and methods for Website in order to include all retreival and parsing and searching in a single class

---
+++ src/Website.java
@@ -0,0 +1,15 @@
+
+
+public class Website {
+	
+	private String url;
+	
+	private String getHTML() {
+		return null;
+	}
+	
+	public boolean searchIfExists(String searchTerm) {
+		String html = getHTML();
+		return false;
+	}
+}
